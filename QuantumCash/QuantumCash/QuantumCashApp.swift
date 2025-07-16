32114ded67fa3fa20b7c6c6260625d91cfe2a414
Mr_Nikitoss <n.moskalenko1996@gmail.com>
2025-07-16 11:55:00 +0300

    Init project

---
+++ QuantumCash/QuantumCash/QuantumCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuantumCashApp.swift
+//  QuantumCash
+//
+//  Created by Nikita Moskalenko on 16.07.25.
+//
+
+import SwiftUI
+
+@main
+struct QuantumCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
